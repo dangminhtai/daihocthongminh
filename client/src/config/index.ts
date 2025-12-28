e09a29bcf8fb553203e428afea9644a895d995db
dangminhtai <dmt826321@gmail.com>
2025-12-28 16:35:19 +0700

    delete prompts in client

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -4,5 +4,3 @@ export * from './api';
 export * from './errors';
 export * from './ui';
 export * from './constants';
-export * from './prompt/majors/gemini_conf';
-export * from './prompt/careers/gemini_conf';
